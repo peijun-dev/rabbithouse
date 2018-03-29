80c5c52777ef9cbd6e1cf0b0ea427dfdca96789c
Peijun/ページュン <contact.zouri.music@gmail.com>
2018-03-29 23:48:52 +0900

    Initial commit

--- rabbithouse/Program.cs
+++ rabbithouse/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace rabbithouse
 {
     class Program
     {
+        static string token = Environment.GetEnvironmentVariable("TelegramCoffee"); 
+        private static readonly TelegramBotClient Bot = new TelegramBotClient(token);
+
         static void Main(string[] args)
         {
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.OnMessageEdited += Bot_OnMessage;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var coffee = "現在準備中です。";
+
+            Console.WriteLine(e.Message.Text);
+
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage)
+            {
+                if (e.Message.Text == "/coffee")
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id,coffee);
+                }
+                else if (e.Message.Text == "/coffee@rabbit_house_bot")
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, coffee);
+                }
+            }
+            
         }
     }
 }
